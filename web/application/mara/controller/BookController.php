c26016c982a77fb3e8cdc68c74eda282b8876c36
aaronzhai <stilliam@163.com>
2018-10-21 00:21:11 +0800

    书本和读后感接口

--- web/application/mara/controller/BookController.php
+++ web/application/mara/controller/BookController.php
@@ -55,7 +55,7 @@ class BookController extends Controller
         $book = $bookDao->getById($id);
 
         Result::returnSuccessResult([
-            'list' => $bookList
+            'bookdetial' => $book
         ]);
     }
 
